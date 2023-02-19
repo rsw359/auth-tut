2c4ff85439a2669a2cb2958a5f8083cb0db4498d
Roger Wells <rsw359@gmail.com>
2023-02-19 21:43:07 +0900

    indv post page created

--- auth-tut/src/App.js
+++ auth-tut/src/App.js
@@ -1,12 +1,13 @@
 import Navbar from "./components/Navbar";
 import "./app.css";
 import Home from "./pages/Home";
+import Post from "./pages/Post";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Home />
+      <Post />
     </div>
   );
 };
---
+++ auth-tut/src/pages/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { posts } from "../data";
+
+const Post = () => {
+  const post = posts[2];
+  return (
+    <div className="post">
+      <img src={post.img} alt="" className="postImg" />
+      <h1 className="postTitle">{post.title}</h1>
+      <p className="postDesc">{post.desc}</p>
+      <p className="longDesc">{post.longDesc}</p>
+    </div>
+  );
+};
+
+export default Post;
