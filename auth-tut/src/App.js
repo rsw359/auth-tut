e1dac2742bdef149893e8305a08252d3fb46d133
Roger Wells <rsw359@gmail.com>
2023-02-20 16:25:31 +0900

    login page initial styles added

--- auth-tut/src/App.js
+++ auth-tut/src/App.js
@@ -2,12 +2,13 @@ import Navbar from "./components/Navbar";
 import "./app.css";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Post />
+      <Login />
     </div>
   );
 };
